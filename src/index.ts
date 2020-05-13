a17f618ef03bca8ede8085105f504f49f64db3e1
Jason Byrne <jason.byrne@flocasts.com>
2020-05-13 13:43:11 -0400

    export more things

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import {
   RenditionSortOrder,
   DynamicChunklistProperties,
 } from "./classes/playlist";
+import { Rendition } from "./classes/rendition";
+import { Segment } from "./classes/segment";
+import { SegmentInfo } from "./classes/segment-info";
 import { Chunklist, ChunklistPruneType } from "./classes/chunklist";
+import { StreamInfo } from "./classes/stream-info";
+import { AudioTrack } from "./classes/audio-track";
 
 export {
   Playlist,
@@ -13,4 +18,9 @@ export {
   DynamicChunklistProperties,
   Chunklist,
   ChunklistPruneType,
+  Rendition,
+  Segment,
+  SegmentInfo,
+  StreamInfo,
+  AudioTrack,
 };
